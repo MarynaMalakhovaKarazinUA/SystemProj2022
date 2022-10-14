8339f014c316d7168f1c0ec522f88be2f6e5b7e6
EduardShamray <shamrai2020ki11@student.karazin.ua>
2022-10-15 01:16:48 +0300

    Add bash script

---
+++ script.sh
@@ -0,0 +1,12 @@
+directory_name=$1
+file_name=$2
+
+if [ -z "$directory_name" ] || [ -z "$file_name" ];
+	then
+		echo "script is empty"
+		exit 0
+fi
+
+find "$directory_name"/ -name "$file_name*" > file.txt
+
+exit 0
