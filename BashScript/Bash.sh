f5f664b2bc7efec17770b9ed91aab2ce86e7282f
User <novikov2020ki11@student.karazin.ua>
2022-11-05 15:04:25 +0200

    Fix info Bash script

--- BashScript/Bash.sh
+++ BashScript/Bash.sh
@@ -2,7 +2,7 @@ directory_name=$1
 file_name=$2
 if [ -z "$directory_name" ] || [ -z "$file_name" ];
 	then
-		echo "Empty"
+		echo "Empty parametrs! Please, enter the parameters in format '<Name script> <Name directory> <Name file>'"
 		exit 0
 fi
 find "$directory_name"/ -name "$file_name*" > file.txt
