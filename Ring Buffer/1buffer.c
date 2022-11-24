4c10c78d2cfe6be28ec40aac9eef67122435cc05
MariiaKharchenko <115325303+MariiaKharchenko@users.noreply.github.com>
2022-11-24 22:19:07 +0200

    Create 1buffer.c

---
+++ Ring Buffer/1buffer.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int getIndex = 0;
+int putIndex = 0;
+
+void addFunction(int buff[],int size, int value) {
+    if (!(putIndex == 0 && getIndex == 0)) {
+        if (putIndex == getIndex) {
+            getIndex++;
+            if (getIndex >= size) {
+                getIndex = 0;
+            }
+        }
+    }
+  
+    putIndex++;
+    if (putIndex > size) {
+        putIndex = 1;
+        if ((putIndex - 1) == getIndex) {
+            getIndex++;
+            if (getIndex >= size) {
+                getIndex = 0;
+            }
+        }
+    }
+    buff[putIndex - 1] = value;
+}
+
+int getFunction(int buff[],int size) {
+    int res = buff[getIndex];
+    buff[getIndex] = 0;
+    getIndex++;
+    if (getIndex >= size) {
+        getIndex = 0;
+    }
+    return res; 
+}
+
+void getAll(int buff[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", buff[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+    printf("Enter the size of buffer: ");
+    scanf(" %d", &size);
+    int buff[size];
+
+    int choice = 0;
+
+    while(1) {
+        printf("putIndex: %d\n", putIndex);
+        printf("getIndex: %d\n", getIndex);
+        printf("1. Add value\n");
+        printf("2. Get value\n");
+        printf("3. Show and clean buffer\n");
+        printf("4. Exit\n");
+        scanf(" %d", &choice);
+
+        switch(choice) {
+            case 1:
+                int value;
+                printf("Enter the value: ");
+                scanf(" %d", &value);   
+                addFunction(buff, size, value);
+                break;
+            case 2:
+                int res = getFunction(buff, size);
+                printf("Value: %d\n", res);
+                break;
+            case 3:
+                getAll(buff, size);
+                break;
+            case 4:
+                exit(1);
+                break;
+            default:                    
+        }
+    }
+}
