f1437468ee22019417427a6291c63c4cb37b8b8b
EduardShamray <shamrai2020ki11@student.karazin.ua>
2022-11-25 03:19:42 +0200

    Add option 'saving to file' and change data type to char

--- BufferTask/buffer2.c
+++ BufferTask/buffer2.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 #include <stdbool.h>
-#include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 typedef struct ring_buffer {
     int size;
     int cur_elemnts_amount;
 
-    int *data;
+    char *data;
     int getIndex;
     int putIndex;
     
@@ -53,7 +58,7 @@ void pushToBuff(ring_buffer *buf, int value) {
 
 int popFromBuff(ring_buffer *buf) {
     int res = buf->data[buf->getIndex];
-    buf->data[buf->getIndex] = 0;
+    buf->data[buf->getIndex] = '\0';
     buf->getIndex++;
     buf->cur_elemnts_amount--;
     if(buf->cur_elemnts_amount == 0)
@@ -64,7 +69,7 @@ int popFromBuff(ring_buffer *buf) {
 
 void getAll(ring_buffer *buf) {
     for (int i = 0; i < buf->size; i++) {
-        printf("%d ", buf->data[i]);
+        printf("%c ", buf->data[i]);
     }
     printf("\n");
 }
@@ -75,10 +80,30 @@ time_t getCurrentDate() {
     return t;
 }
 
+void showCurrentTime(ring_buffer *buf) {
+    time_t t = buf->time;
+    printf("Current date and time is : %s", ctime(&t));
+}
+
+void saveToFile(ring_buffer *buf) {
+    int file;
+    file = open("bufferFile.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (file < 0) {
+		printf("Failed to open destiantion file: %s\n", strerror(errno));
+		return;
+	}
+    for (int i = 0; i < buf->size; i++) {
+        if (buf->data[i] != '\0') {
+            write(file, &buf->data[i], 1);
+        }
+    }   
+    close(file);
+}
+
 ring_buffer* buff_init(int size) {
     ring_buffer *buff = malloc(sizeof(ring_buffer));
     buff->size = size;
-    buff->data = (int *) malloc(size * sizeof(int));
+    buff->data = (char *) malloc(size * sizeof(char));
     buff->getIndex = 0;
     buff->putIndex = 0;
     buff->putIndex_was_behind = false;
@@ -97,40 +122,43 @@ int main() {
     int choice = 0;
 
     while(1) {
+        buff->time = getCurrentDate();
         printf("1.Add value\n");
         printf("2.Get value\n");
         printf("3.Show the buffer\n");
-        printf("4.Exit\n");
+        printf("4.Get current time\n");
+        printf("0.Save and exit\n");
         printf(">> ");
         scanf(" %d", &choice);
 
         switch(choice) {
             case 1:
-                buff->time = getCurrentDate();
-                int user_value;
+                char user_value;
                 printf("Enter the value: ");
-                scanf(" %d", &user_value);   
+                scanf(" %c", &user_value);   
                 pushToBuff(buff, user_value);
                 buff->record_id++;
                 break;
             case 2:
-                buff->time = getCurrentDate();
                 if (isEmpty(buff) == 0) {
                     printf("No elements in buffer\n");
                     break;
                 }
-                int res = popFromBuff(buff);
-                printf("Value: %d\n", res);
+                char res = popFromBuff(buff);
+                printf("Value: %c\n", res);
                 break;
             case 3:
-                buff->time = getCurrentDate();
                 getAll(buff);
                 break;
             case 4:
-                buff->time = getCurrentDate();
-                exit(1);
+                showCurrentTime(buff);
+                break;     
+            case 0:
+                saveToFile(buff);
+                exit(0);
                 break;
-            default:                    
+            default:
+                printf("There isn`t such option\n");                    
         }
     }
 }
