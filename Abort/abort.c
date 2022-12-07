b520eac46953cb694ef0142131ed230ff54c0ac1
Misha <116115869+Misha-Pytaichuk@users.noreply.github.com>
2022-12-07 22:07:32 +0200

    Add abort

---
+++ Abort/abort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+static void daemon()
+{
+    pid_t pid;
+    pid = fork();
+
+    if(pid != 0) abort();
+
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    pid = fork();
+
+    if(pid != 0) abort();
+
+    umask(0);
+
+    chdir("/");
+
+    int num;
+
+    for (num = sysconf(_SC_OPEN_MAX); num >= 0; num--)
+    {
+      close(num);
+    }
+
+    openlog("daemon", LOG_PID, LOG_DAEMON);
+}
+
+int main()
+{
+    daemon();
+
+    while (1)
+    {
+        syslog(LOG_NOTICE, "Daemon started...");
+        sleep(20);
+        break;
+    }
+
+    syslog(LOG_NOTICE, "Daemon terminated.");
+    abort();
+
+    return EXIT_SUCCESS;
+}
