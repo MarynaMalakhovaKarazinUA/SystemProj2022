9a9e2ff0f59dc4b8592b1c2eca40d4c8be283dc8
FogoReed <doterhd03@gmail.com>
2022-12-19 23:43:43 +0200

    Add task6upd

---
+++ task6upd/file.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+pid_t backDaemon() {
+    pid_t pid;
+
+    pid = fork();
+
+    if(pid < 0) {
+        printf("Помилка FORK!\n");
+        exit(1);
+    }
+
+    chdir("/");
+    openlog("daemon", LOG_PID, LOG_DAEMON);
+
+    return pid;
+}
+
+int main() {
+    pid_t pid = backDaemon();
+
+    if(pid == 0) {
+        while(1) {
+            syslog(LOG_NOTICE, "Початок роботи Демону");
+            sleep(35);
+
+            break;
+        }
+    } else {
+        int ch;
+
+        while(ch != 0) {
+            printf("\nВведіть \"0\" щоб закінчити программу\n >>> ");
+            scanf(" %d", &ch);
+        }
+
+        kill(pid, SIGKILL);
+
+        syslog(LOG_NOTICE, "Завершення роботи Демону");
+        closelog();
+    }
+
+    return 0;
+}
